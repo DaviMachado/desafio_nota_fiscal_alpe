6705413a66fe55f8546d0f299be1c33e6599f415
Davi Rodrigues de Souza Machado <davisouzamachado@outlook.com>
2024-05-15 17:43:33 -0300

    feat: commit inicial do projeto.

---
+++ src/main/java/com/example/desafio_nota_fiscal_alpe/DesafioNotaFiscalAlpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_nota_fiscal_alpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNotaFiscalAlpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNotaFiscalAlpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_nota_fiscal_alpe/DesafioNotaFiscalAlpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_nota_fiscal_alpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNotaFiscalAlpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
