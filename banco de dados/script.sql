d01c68717f8c0ee3c0a44ea60ca8922c93200508
Davi Rodrigues de Souza Machado <davisouzamachado@outlook.com>
2024-05-16 18:35:53 -0300

    docs: inclusão do arquivo SQL no projeto.

---
+++ banco de dados/script.sql
@@ -0,0 +1,36 @@
+----------------
+----------------
+----------------
+
+-- Criando a tabela CLIENTE
+CREATE TABLE CLIENTE (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    NOME NVARCHAR(100),
+    CPF NVARCHAR(11)
+);
+
+
+-- Criando a tabela NOTA_FISCAL
+CREATE TABLE NOTA_FISCAL (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    NUMERO_NOTA INT,
+    DATA_NOTA DATETIME,
+    FORMA_PAGAMENTO NVARCHAR(50),
+    CLIENTE_ID INT,
+    FOREIGN KEY (CLIENTE_ID) REFERENCES CLIENTE(ID)
+);
+
+
+-- Insert necessario do cliente
+INSERT INTO CLIENTE (NOME, CPF)
+VALUES ('davi', '82272683070');
+
+
+--DROP TABLE NOTA_FISCAL;
+--DROP TABLE CLIENTE;
+
+
+
+
+
+
